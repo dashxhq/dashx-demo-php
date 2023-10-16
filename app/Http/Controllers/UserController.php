56588c71dae20fe9265009e897733a3f52acacf3
Ajay Kumar <ajay@keepworks.com>
2023-10-16 18:23:09 +0530

    Added dashx config

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests\UserUpdateRequest;
 use App\Models\User;
 
+use DashX;
+
 class UserController extends Controller
 {
     /**
@@ -42,7 +44,31 @@ class UserController extends Controller
      */
     public function contact(ContactRequest $request)
     {
-        // TODO: hit dx.deliver email
+        DashX::deliver('email', [
+            'content' => [
+                'name' => 'Contact us',
+                'from' => 'noreply@dashxdemo.com',
+                'to' => [$request->email, 'sales@dashx.com'],
+                'subject' => 'Contact Us Form',
+                'html_body' => <<<HTML
+                    <mjml>
+                        <mj-body>
+                            <mj-section>
+                                <mj-column>
+                                    <mj-divider border-color='#F45E43'></mj-divider>
+                                    <mj-text>Thanks for reaching out! We will get back to you soon!</mj-text>
+                                    <mj-text>Your feedback: </mj-text>
+                                    <mj-text>Name: $request->name</mj-text>
+                                    <mj-text>Email: $request->email</mj-text>
+                                    <mj-text>Feedback: $request->feedback</mj-text>
+                                    <mj-divider border-color='#F45E43'></mj-divider>
+                                </mj-column>
+                            </mj-section>
+                        </mj-body>
+                    </mjml>
+                HTML
+            ]
+        ]);
 
         return response()->json([
             'message' => 'Thanks for reaching out! We will get back to you soon.'
